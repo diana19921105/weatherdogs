7fa35aec63e33da74019e4ddaa0d8a73ae01c614
Diána Szántó <szanto.diana.1992@gmail.com>
2021-05-30 10:37:21 +0200

    tiny changes

--- src/myscript.js
+++ src/myscript.js
@@ -10,16 +10,26 @@
 
 
         function getWeatherByLocation(position) {
-            const Url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&appid=19be452deb7864ee0101b533eea372cf';
+            const Url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position + '&lon=' + position + '&appid=19be452deb7864ee0101b533eea372cf';
             fetch(Url)
                 .then(data => {
                     return data.json()
                 })
                 .then(res => {
                     console.log(res)
+                    const giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=GnMcn23xwvNLTkuY4JiCKKA3kCbgG9Ju&q=' + res + '&limit=25&offset=0&rating=g&lang=en';
+
+                    fetch(giphyUrl)
+                        .then(data => {
+                            return data.json()
+                        })
+                        .then(res => {
+                           console.log(res);
+                           document.getElementById("giphy").setAttribute('src', res);
+                        });
                 });
+
+
         }
-
     }
-
 )()
