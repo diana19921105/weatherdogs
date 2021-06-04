04872e048215b34024737a598204c493cadf3a95
Diána Szántó <szanto.diana.1992@gmail.com>
2021-06-04 13:58:46 +0200

    fix the code, add some new features

--- src/myscript.js
+++ src/myscript.js
@@ -10,22 +10,23 @@
 
 
         function getWeatherByLocation(position) {
-            const Url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position + '&lon=' + position + '&appid=19be452deb7864ee0101b533eea372cf';
+            const Url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&appid=19be452deb7864ee0101b533eea372cf';
             fetch(Url)
                 .then(data => {
                     return data.json()
                 })
                 .then(res => {
-                    console.log(res)
-                    const giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=GnMcn23xwvNLTkuY4JiCKKA3kCbgG9Ju&q=' + res + '&limit=25&offset=0&rating=g&lang=en';
+                    const giphyUrl = 'https://api.giphy.com/v1/gifs/search?api_key=GnMcn23xwvNLTkuY4JiCKKA3kCbgG9Ju&q=' + res.weather[0].main + '+dog&limit=25&offset=0&rating=g&lang=en';
+                    document.getElementById("location").innerHTML = res.name;
+                    document.getElementById("weather").innerHTML = res.weather[0].main;
+                    document.getElementById("temp").innerHTML = (res.main.temp - 273.15).toFixed(1);
 
                     fetch(giphyUrl)
                         .then(data => {
                             return data.json()
                         })
                         .then(res => {
-                           console.log(res);
-                           document.getElementById("giphy").setAttribute('src', res);
+                           document.getElementById("giphy").setAttribute('src', res.data[0].embed_url);
                         });
                 });
 
